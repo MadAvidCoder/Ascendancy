ffb480a5f662742edfbe031239d9853ccea0fd9a
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-07-29 22:02:45 +1000

    Make fire damage player
    Added logic to the fire to be able to deal damage to player on contact.

--- fire.gd
+++ fire.gd
@@ -1,5 +1,10 @@
-extends AnimatedSprite2D
+extends Area2D
 
 func _ready() -> void:
 	await get_tree().create_timer(randf()).timeout
-	play()
+	$Sprite2D.play()
+
+func _process(_delta: float) -> void:
+	for body in self.get_overlapping_bodies():
+		if body.name == "Player":
+			body.hit(self)
